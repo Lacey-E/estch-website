66d099c05dde23b65e89426a29b1f348e9bf3b97
Lacey-E <buchiezim@gmail.com>
2023-12-12 23:26:02 -0700

    up

--- components/Button.jsx
+++ components/Button.jsx
@@ -4,6 +4,7 @@ const Button = ({ children,  onClick }) => {
   return (
     <button className='specialbtn'  onClick={onClick}>
       {children}
+      <h2>keep connected</h2>
     </button>
   );
 };
